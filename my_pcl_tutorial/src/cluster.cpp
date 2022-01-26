3bbe2ac6d6990e3b804c28a1cec88af3c7c4c96c
Gonnnnn <geonkim@yonsei.ac.kr>
2022-01-26 23:08:28 +0900

    changed cluster.cpp so that it will publish every cluster for the moment. Bounding boxes will be added

--- my_pcl_tutorial/src/cluster.cpp
+++ my_pcl_tutorial/src/cluster.cpp
@@ -69,46 +69,93 @@ void input (const sensor_msgs::PointCloud2ConstPtr& scan)
   ec.setInputCloud(src);
   ec.extract(cluster_indices);
 
-  std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> clusters;
+
+/*-------------------------기존 코드-------------------------------------------------*/
+//   std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> clusters;
+//   // 아래와 같이 구현했을 때, iterator를 이용해 for문을 돌리는 것과 같은 방식으로 연산이 되는지는 아직 모르겠다.
+//   // iterator을 이용할 경우 ++it와 같은 후위 증감을 사용한다.
+//   // 전위증감은 값만 증가시키면 되는 것에 비해, 후위증감을 하게 되면 임시 객체를 만들어 현재의 값을 저장해 두어야 하는데, 여기서 부하가 발생하기 때문이다.
+//   // int나 int*같은 원시 타입은 대입 없이 단독으로 쓰이면 i++도 임시로 값을 저장하는 일 없이 최적화되어 ++i와 같은 코드로 번역되지만,
+//   // 반복자는 그런 최적화가 없어 사용자가 ++i로 적어야 불필요한 비용을 피할 수 있다.
+//   // 따라서, for (pcl::PointIndices getIndices: cluster_indices) 와 같은 방식이 위와 같은 방식으로 for문을 돌리는지 확인해보자!
+//   for (pcl::PointIndices getIndices: cluster_indices)
+//     {
+//         // every loop, initialize a cloud
+//         pcl::PointCloud<pcl::PointXYZI>::Ptr clusters (new pcl::PointCloud<pcl::PointXYZI>);
+//         // get each index in cluster in cluster_indices and push it into the cloud above
+//         for (int index : getIndices.indices)
+//             clusters->points.push_back(src->points[index]);
+//         // set width, height and such --> https://pointclouds.org/documentation/singletonpcl_1_1_point_cloud.html
+//         // width
+//         //// it specifies the total number of points in the cloud
+//         //// or the width (total number of points in a row) of an organized PC dataset
+//         // height
+//         //// it specifies the height (total number of rows) of an organized PC dataset
+//         //// it is set to 1 for unorganized datasets(thus used to check whether a dataset is organized or not)
+//         // is_dense
+//         //// specifies if all the data in points is finite (true), or whether it might contain Inf/NaN values (false)
+//         clusters->width = clusters->points.size();
+//         clusters->height = 1;
+//         clusters->is_dense = true;
+//         // push the cloud into clusters(a vector containing pcl::PointCloud<PointT>::Ptr)
+//         clusters.push_back(clusters);
+//     }
+
+// ///////////////////////clustering 끝///////////////////////
+
+//   try { 
+//     // 예외가 발생하면 예외를 던지는 영역
+//     if (clusters.size() == 0) throw clusters;
+//     pcl::PointCloud<pcl::PointXYZI>::Ptr temp = clusters[0];
+//     pcl::PointCloud<pcl::PointXYZI> cloud1 = *temp;
+
+//     pcl::PCLPointCloud2 clustered_cloud;
+//     pcl::toPCLPointCloud2(cloud1, clustered_cloud);
+//     sensor_msgs::PointCloud2 clustered_output;
+//     pcl_conversions::fromPCL(clustered_cloud, clustered_output);
+
+//     clustered_output.header.frame_id = "velodyne";
+//     pub.publish(clustered_output);
+//   } catch (std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> exception) {// 던져진 예외를 잡는 영역
+//   // 예외 처리 영역
+//     std::cout << "there is no cluster" << std::endl;
+//   }
+
+
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr clusters(new pcl::PointCloud<pcl::PointXYZI>);
+  // 아래와 같이 구현했을 때, iterator를 이용해 for문을 돌리는 것과 같은 방식으로 연산이 되는지는 아직 모르겠다.
+  // iterator을 이용할 경우 ++it와 같은 후위 증감을 사용한다.
+  // 전위증감은 값만 증가시키면 되는 것에 비해, 후위증감을 하게 되면 임시 객체를 만들어 현재의 값을 저장해 두어야 하는데, 여기서 부하가 발생하기 때문이다.
+  // int나 int*같은 원시 타입은 대입 없이 단독으로 쓰이면 i++도 임시로 값을 저장하는 일 없이 최적화되어 ++i와 같은 코드로 번역되지만,
+  // 반복자는 그런 최적화가 없어 사용자가 ++i로 적어야 불필요한 비용을 피할 수 있다.
+  // 따라서, for (pcl::PointIndices getIndices: cluster_indices) 와 같은 방식이 위와 같은 방식으로 for문을 돌리는지 확인해보자!
   for (pcl::PointIndices getIndices: cluster_indices)
     {
         // every loop, initialize a cloud
-        pcl::PointCloud<pcl::PointXYZI>::Ptr cloudCluster (new pcl::PointCloud<pcl::PointXYZI>);
         // get each index in cluster in cluster_indices and push it into the cloud above
         for (int index : getIndices.indices)
-            cloudCluster->points.push_back(src->points[index]);
-        // set width, height and such --> https://pointclouds.org/documentation/singletonpcl_1_1_point_cloud.html
-        // width
-        //// it specifies the total number of points in the cloud
-        //// or the width (total number of points in a row) of an organized PC dataset
-        // height
-        //// it specifies the height (total number of rows) of an organized PC dataset
-        //// it is set to 1 for unorganized datasets(thus used to check whether a dataset is organized or not)
-        // is_dense
-        //// specifies if all the data in points is finite (true), or whether it might contain Inf/NaN values (false)
-        cloudCluster->width = cloudCluster->points.size();
-        cloudCluster->height = 1;
-        cloudCluster->is_dense = true;
-        // push the cloud into clusters(a vector containing pcl::PointCloud<PointT>::Ptr)
-        clusters.push_back(cloudCluster);
+            clusters->points.push_back(src->points[index]);
     }
+  clusters->width = clusters->points.size();
+  clusters->height = 1;
+  clusters->is_dense = true;
+
 ///////////////////////clustering 끝///////////////////////
 
   try { 
     // 예외가 발생하면 예외를 던지는 영역
-    if (clusters.size() == 0) throw clusters;
-    pcl::PointCloud<pcl::PointXYZI>::Ptr temp = clusters[0];
-    pcl::PointCloud<pcl::PointXYZI> cloud1 = *temp;
+    if (clusters->size() == 0) throw clusters;
     pcl::PCLPointCloud2 clustered_cloud;
-    pcl::toPCLPointCloud2(cloud1, clustered_cloud);
+    pcl::toPCLPointCloud2(*clusters, clustered_cloud);
     sensor_msgs::PointCloud2 clustered_output;
     pcl_conversions::fromPCL(clustered_cloud, clustered_output);
 
     clustered_output.header.frame_id = "velodyne";
     pub.publish(clustered_output);
-  } catch (std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> exception) {// 던져진 예외를 잡는 영역
+  } catch (pcl::PointCloud<pcl::PointXYZI> exception) {// 던져진 예외를 잡는 영역
   // 예외 처리 영역
-  std::cout << "there is no cluster" << std::endl;
+    std::cout << "there is no cluster" << std::endl;
   }
 }
 
